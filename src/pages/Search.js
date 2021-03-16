c772ea9ae14c167aa68b4b271aae21a2560cd85b
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-16 19:56:00 -0400

    Search fixes

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div className="container" >
-            <div className="row">
-                <h1 style={{padding: "4rem 0", margin: 'auto'}}>Search for Properties</h1>
+        <div className="container " >
+            <div className="p-3 text-center">
+                <h1 style={{paddingTop: "6rem"}}>Search for Properties</h1>
                 <iframe src="https://matrix.bnymls.com/Matrix/public/IDX.aspx?idx=3c10134" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
             </div>
         </div>
