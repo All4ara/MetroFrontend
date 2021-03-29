e221741560f6bc92d4d60442c45a08774ca1e64d
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-29 16:48:00 -0400

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,12 @@ import logo from '../images/metrologo2.png'
 
 const About = () => {
     return (
-        <div className="container text-left" style={{paddingTop: '10rem', height: '100vh'}}>
+        <div className="container text-left" style={{paddingTop: '10rem', height: '100%'}}>
             <div>
                 <img style={{float: 'left', margin: '20px 30px 30px 0', width: '40vw', height: '35vh'}} src={logo}/>
+                <p>Metropolitan Brokers is a developing real estate company in Brooklyn New York which specializes in residential sales and rentals, while also performing many commercial transactions as well. Our team is young, motivated and there to get the job done for the client. Our number one priority is to make our customers happy. Feel free to each out to us today to make us the next real estate company you use.</p>
+                <p>Metropolitan Brokers is a developing real estate company in Brooklyn New York which specializes in residential sales and rentals, while also performing many commercial transactions as well. Our team is young, motivated and there to get the job done for the client. Our number one priority is to make our customers happy. Feel free to each out to us today to make us the next real estate company you use.</p>
+                <p>Metropolitan Brokers is a developing real estate company in Brooklyn New York which specializes in residential sales and rentals, while also performing many commercial transactions as well. Our team is young, motivated and there to get the job done for the client. Our number one priority is to make our customers happy. Feel free to each out to us today to make us the next real estate company you use.</p>
             </div>
         </div>
     )
