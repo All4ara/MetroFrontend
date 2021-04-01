0d62e150522937b488fbbc89f48b7c05936cacdc
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-04-01 16:09:34 -0400

    touch ups

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div className="container " >
+        <div className="container py-5" >
             <div className="p-4 text-center">
-                <h1 style={{paddingTop: "6rem", fontFamily: "Playfair Display SC"}}>Search for Properties</h1>
+                <h1 style={{padding: "6rem 0", fontFamily: "Playfair Display SC"}}>Search for Properties</h1>
                 <iframe src="https://matrix.bnymls.com/Matrix/public/IDX.aspx?idx=3c10134" width="100%" height="800px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
             </div>
         </div>
